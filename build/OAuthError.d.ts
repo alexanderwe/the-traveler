58eaae969b58da26d87a1d07e5f95b25eed33834
Alexander <weiss_alex@gmx.net>
2017-09-23 21:20:41 +0200

    Test if codecov works if the build directory is present

---
+++ build/OAuthError.d.ts
@@ -0,0 +1,3 @@
+export default class OAuthError extends Error {
+    constructor(message: string);
+}
---
+++ build/interfaces.d.ts
@@ -0,0 +1,102 @@
+import { BungieMembershipType, ComponentType, DestinyActivityModeType, DestinyStatsGroupType, PeriodType } from './enums';
+/**
+ * Interface for the common API response object
+ * @interface
+ */
+export interface IAPIResponse {
+    Response: object;
+    ErrorCode: string;
+    ThrottleSeconds?: string;
+    ErrorStatus?: string;
+    Message?: string;
+    MessageData: object;
+}
+/**
+ * Interface for defining an object for the Traveler class
+ * @interface
+ */
+export interface IConfig {
+    apikey: string;
+    userAgent: string;
+    oauthClientId?: string;
+    oauthClientSecret?: string;
+    debug?: boolean;
+}
+/**
+ * Interface for defining an object in case of an item action request to the API
+ * @interface
+ */
+export interface IDestinyItemActionRequest {
+    itemId?: string;
+    characterId: string;
+    membershipType: BungieMembershipType;
+}
+/**
+ * Interface for defining an object in case of an item set action request to the API
+ * @interface
+ */
+export interface IDestinyItemSetActionRequest {
+    itemIds?: string[];
+    characterId: string;
+    membershipType: BungieMembershipType;
+}
+/**
+ * Interface for defining an object in oder to set the lock state of an item in the inventory
+ * @interface
+ */
+export interface IDestinyItemStateRequest {
+    state: boolean;
+    itemId: string;
+    characterId: string;
+    membershipType: BungieMembershipType;
+}
+/**
+ * Interface for defining an object in order to transfer it from inventory to vault or vice versa
+ * @interface
+ */
+export interface IDestinyItemTransferRequest {
+    itemReferenceHash: string;
+    stackSize: number;
+    transferToVault: boolean;
+    itemId: string;
+    characterId: string;
+    membershipType: BungieMembershipType;
+}
+/**
+ * Interface for defining an object for the endpoint query strings
+ * @interface
+ */
+export interface IQueryStringParameters {
+    components?: ComponentType[];
+    modes?: DestinyActivityModeType[];
+    mode?: DestinyActivityModeType;
+    maxtop?: number;
+    statid?: string;
+    page?: number;
+    dayend?: string;
+    daystart?: string;
+    groups?: DestinyStatsGroupType[];
+    periodType?: PeriodType;
+    count?: number;
+    [key: string]: any;
+}
+/**
+ * Interface for defining an object for the OAuth process
+ * @interface
+ */
+export interface IOAuthConfig {
+    clientId?: string;
+    clientSecret?: string;
+}
+/**
+ * Interface for defining an object for the OAuth response
+ * @interface
+ */
+export interface IOAuthResponse {
+    access_token: string;
+    token_type: string;
+    expires_in: number;
+    refresh_token?: string;
+    refresh_expires_in?: number;
+    membership_id: string;
+}
