feb912537f03b4549abadc5f6e411875e92f7caa
Alexander Weiß <weiss_alex@gmx.net>
2017-12-29 01:26:04 +0100

    Removed ReportOffensivePostGameCarnageReportPlayer method due to not obtainable oauth scope for this endpoint

--- src/Traveler.ts
+++ src/Traveler.ts
@@ -381,32 +381,6 @@ export default class Traveler {
     }
 
     /**
-     * Report a player that you met in an activity that was engaging in ToS-violating activities. Both you and the offending player must have played in the activityId passed in. 
-     * Please use this judiciously and only when you have strong suspicions of violation, pretty please.
-     * @async
-     * @param activityId The ID of the activity where you ran into the brigand that you're reporting.
-     * @return {Promise.IAPIResponse<IDestinyPostGameCarnageReportData>} When fulfilled returns an object containing the carnage report for the specified activity
-     */
-    public reportOffensivePostGameCarnageReportPlayer(activityId: string, destinyReportOffensePgcrRequest: IDestinyReportOffensePgcrRequest): Promise<IAPIResponse<number>> {
-        if (this.oauth !== undefined) {
-            this.oauthOptions.body = destinyReportOffensePgcrRequest;
-            this.options.uri = `${this.apibase}/Stats/PostGameCarnageReport/${activityId}/Report/`;
-            this.oauthOptions.json = true;
-            return new Promise<IAPIResponse<number>>((resolve, reject) => {
-                this.httpService.post(this.oauthOptions)
-                    .then((response: IAPIResponse<number>) => {
-                        resolve(response);
-                    })
-                    .catch((err) => {
-                        reject(err);
-                    });
-            });
-        } else {
-            throw new OAuthError('You have to use OAuth to access this endpoint. Your oauth object is this: ' + JSON.stringify(this.oauth) + ' Please use traveler.oauth = yourOauthObject to set it.');
-        }
-    }
-
-    /**
      * Get historical stats definitions. This contains the values for the `<br` key.
      * @async
      */
